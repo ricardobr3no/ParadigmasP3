cc51e423293b80d151856bccb3690ecbab029fa6
Gustavo de Oliveira <gustavorei13@gmail.com>
2025-02-18 02:23:41 -0300

    Criando a classe pedido
    - Tem atributos de:
       * codigoPedido, codigoProduto, valor,
    quantidade e preco total, ainda penso em colocar a data tbm
    - tem um construtor recheado
    - preco total é um valor calculado
    - CODIGO É INTEIRO A PARTIR DE AGORA

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,14 +1,53 @@
-public class Pedido {
+public class Pedido{
+    private int codigoPedido;
+    private int codigoProduto;
+    private double valor;
+    private int quantidade;
+    private double total;
 
-    double total = 0;
-
-    void fazerPedido(Produto produto, int quantidade, double porcentagemDesconto) {
-        // lancar erro se quantidade < 0 ou se produto nao tiver disponivel
-        produto.aplicaDesconto(porcentagemDesconto);
-        total += produto.getValor() * quantidade;
+    public Pedido(int codigoPedido, int codigoProduto, int quantidade, double valor) {
+        this.quantidade = quantidade;
+        this.valor = valor;
+        this.codigoPedido = codigoPedido;
+        this.codigoProduto = codigoProduto;
+        this.total = valor * quantidade;
     }
 
-    public static void main(String[] args) {
+    public int getCodigoPedido() {
+        return codigoPedido;
+    }
 
+    public int getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Pedido: " + this.codigoPedido +
+                " | Produto: " + this.codigoProduto +
+                " | Quantidade: " + this.quantidade +
+                " | Preço unitário: R$" + this.valor +
+                " | Total: R$" + total;
     }
 }
