adf02f2e841b8a982d0140065effd1513f68323e
Gustavo de Oliveira <gustavorei13@gmail.com>
2025-02-18 02:20:56 -0300

    colocando tudo em comentario

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,34 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        String codigo = "123";
+        /*
+        int codigo =123;
         String nome = "paracetamol";
         double preco = 13.40;
 
         Produto p = new Medicamento();
         p.setCodigo(codigo);
-        p.setNome(nome);
+        p.setItem(nome);
         p.setValor(preco);
 
-        String codigo2 = "1333";
+        int codigo2 = 1333;
         String nome2 = "buscopan";
         double preco2 = 8.80;
 
         Produto p2 = new Medicamento();
         p2.setCodigo(codigo2);
-        p2.setNome(nome2);
+        p2.setItem(nome2);
         p2.setValor(preco2);
 
         Estoque e = new Estoque();
         e.cadastrarProduto(p);
         e.cadastrarProduto(p2);
         e.listarProdutos();
-        e.alterarProduto("1333", "vitamina c", 8.80);
+        e.alterarProduto(1333, "vitamina c", 8.80);
         e.listarProdutos();
-        e.removerProduto("123");
+        e.removerProduto(123);
         e.listarProdutos();
+        */
+
     }
 }
